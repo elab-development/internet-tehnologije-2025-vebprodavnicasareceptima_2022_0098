42d7f58f70a6ba8c63d0684f4aacfcb5f1f388c8
DusicaLjubicic <dusica03va@gmail.com>
2026-01-29 10:58:34 +0100

    App Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
+import OrderDetails from './pages/OrderDetails';
+import RecipeDetails from './pages/RecipeDetails';
+import Profile from './pages/Profile';
+
 function App() {
-  return <>RecipesShop</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipe/:recipeId' element={<RecipeDetails />} />
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order/:orderId' element={<OrderDetails />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
